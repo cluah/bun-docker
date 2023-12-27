18f34616691186b69b4ae960355a2fcfde435f50
camilo <122648346+cluah@users.noreply.github.com>
2023-12-27 00:24:18 -0300

    .

--- app.ts
+++ app.ts
@@ -1,10 +1,34 @@
 const PORT: number = +(process.env.PORT || 8081);
 const NODE_ENV = process.env.NODE_ENV ?? "development";
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 
 const server = Bun.serve({
   port: PORT,
-  fetch() {
-    return new Response("Welcome to Bun!");
+  async fetch() {
+    const browser = await puppeteer
+    .use(StealthPlugin())
+    .launch({ headless: 'new' });
+
+  const page = await browser.newPage();
+  await page.goto('https://www.diariooficial.interior.gob.cl/');
+
+  try {
+    await page.waitForSelector('#tramites', { timeout: 3000 });
+    const content = await page.content()
+
+    return new Response(content);
+  } catch (err) {
+    // await page.screenshot({
+    //   path: Date.now() + '.png',
+    //   fullPage: true,
+    // });
+
+    // throw new Error();
+    return new Response(":(");
+    // return c.text(':(');
+  }
+
   },
 });
 
